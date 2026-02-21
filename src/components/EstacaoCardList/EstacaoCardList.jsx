cea612e8bcb2730f3adf3d1951f29bb4fc0cefe0
VMonteiro <valmirmjunior@gmail.com>
2026-02-21 15:50:10 -0300

    ordenação das estações por disponibilidade

--- src/components/EstacaoCardList/EstacaoCardList.jsx
+++ src/components/EstacaoCardList/EstacaoCardList.jsx
@@ -1,15 +1,40 @@
+import { useState, useEffect } from "react";
 import EstacaoCard from "../EstacaoCard/EstacaoCard";
 import { catalogoEstacoes } from "../../data/estacoes";
+import { buscarDadosInstantaneosEstacao } from "../../utils/buscarDados";
 import "./EstacaoCardList.css";
 
 export default function EstacaoCardList() {
+  const [estacoesOrdenadas, setEstacoesOrdenadas] = useState(
+    catalogoEstacoes.getEstacoesAtivas()
+  );
+
+  useEffect(() => {
+    const estacoes = catalogoEstacoes.getEstacoesAtivas();
+    Promise.all(
+      estacoes.map((estacao) =>
+        buscarDadosInstantaneosEstacao(estacao.id).then((dados) => ({
+          estacao,
+          online: !!dados,
+        }))
+      )
+    ).then((resultados) => {
+      const ordenadas = resultados
+        .sort((a, b) => Number(b.online) - Number(a.online))
+        .map((r) => r.estacao);
+      setEstacoesOrdenadas(ordenadas);
+    });
+  }, []);
+
   return (
     <div className="estacao-card-list-wrapper">
       {/* <h2 className="estacao-card-list-titulo">Estações Ativas</h2> */}
       <div className="estacao-card-list-container">
-        {catalogoEstacoes.getEstacoesAtivas().map((estacao) =>
-            <EstacaoCard key={estacao.id+"_card"} stationId={estacao.id}>{estacao.bairro}</EstacaoCard>
-        )}
+        {estacoesOrdenadas.map((estacao) => (
+          <EstacaoCard key={estacao.id + "_card"} stationId={estacao.id}>
+            {estacao.bairro}
+          </EstacaoCard>
+        ))}
       </div>
     </div>
   );
--- src/components/EstacaoMenu/EstacaoMenu.jsx
+++ src/components/EstacaoMenu/EstacaoMenu.jsx
@@ -1,14 +1,37 @@
+import { useState, useEffect } from "react";
 import EstacaoBotao from "../EstacaoBotao/EstacaoBotao";
 import { catalogoEstacoes } from "../../data/estacoes";
+import { buscarDadosInstantaneosEstacao } from "../../utils/buscarDados";
 import "./EstacaoMenu.css";
 
 export default function EstacaoMenu({ estacaoSelecionada }) {
+  const [estacoesOrdenadas, setEstacoesOrdenadas] = useState(
+    catalogoEstacoes.getEstacoesAtivas()
+  );
+
+  useEffect(() => {
+    const estacoes = catalogoEstacoes.getEstacoesAtivas();
+    Promise.all(
+      estacoes.map((estacao) =>
+        buscarDadosInstantaneosEstacao(estacao.id).then((dados) => ({
+          estacao,
+          online: !!dados,
+        }))
+      )
+    ).then((resultados) => {
+      const ordenadas = resultados
+        .sort((a, b) => Number(b.online) - Number(a.online))
+        .map((r) => r.estacao);
+      setEstacoesOrdenadas(ordenadas);
+    });
+  }, []);
+
   return (
     <>
       <div className="container-menu-estacoes">
       <h2 className="titulo-selecao-estacao">Selecione uma Estação</h2>
         <menu className="scroll-menu-estacoes">
-          {catalogoEstacoes.getEstacoesAtivas().map((estacao) => (
+          {estacoesOrdenadas.map((estacao) => (
             <EstacaoBotao
               key={estacao.id}
               stationId={estacao.id}
