97963f08cdf382dbbcf4355a2c0dabd6fd1a9cd8
VMonteiro <valmirmjunior@gmail.com>
2026-02-22 12:05:49 -0300

    ajuste da do cálculo do IC para seguir o modelo NOAA fielmente

--- src/utils/heatIndexCalculator.js
+++ src/utils/heatIndexCalculator.js
@@ -1,5 +1,6 @@
 /**
  * Calcula o Índice de Calor (Heat Index) baseado na equação da NOAA.
+ * https://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml#HIadjustments
  * @param {number} temperaturaC - Temperatura em graus Celsius.
  * @param {number} umidadeRelativa - Umidade Relativa (0 a 100).
  * @returns {number} - Índice de Calor calculado em Celsius.
@@ -10,13 +11,23 @@ export const calculateHeatIndex = (temperaturaC, umidadeRelativa) => {
   // Celsius para Fahrenheit
   const temperaturaF = (temperaturaC * 9) / 5 + 32;
 
-  // Justificativa para os ajustes pode ser encontrada em: https://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml#HIadjustments
+  // Primeiro calcula utilizando o modelo simplificado e calcula a média
+  // entre o resultado simplificado e a temperatura real.
+  let indiceCalorSimplificadoF =
+    0.5 *
+    (temperaturaF +
+      61.0 +
+      (temperaturaF - 68.0) * 1.2 +
+      umidadeRelativa * 0.094);
 
-  // Se a temperatura for menor que 80°F (aprox. 26.7°C),
-  // o índice de calor é igual à própria temperatura.
-  if (temperaturaF < 80) {
-    const categoriaCalor = getCategoriaIndiceCalor(temperaturaC);
-    return { indiceCalor: Number(temperaturaC).toFixed(1), ...categoriaCalor };
+  let indiceCalorMediaF = (indiceCalorSimplificadoF + temperaturaF) / 2;
+
+  // Se o resultado simplificado for menor que 80°F, retorna a média convertida para Celsius
+  if (indiceCalorMediaF < 80) {
+    const indiceCalorC = ((indiceCalorMediaF - 32) * 5) / 9;
+    const categoriaCalor = getCategoriaIndiceCalor(indiceCalorC);
+
+    return { indiceCalor: indiceCalorC.toFixed(1), ...categoriaCalor };
   }
 
   // Equação de Regressão Múltipla da NOAA
@@ -47,16 +58,6 @@ export const calculateHeatIndex = (temperaturaC, umidadeRelativa) => {
   else if (umidadeRelativa > 85 && temperaturaF >= 80 && temperaturaF <= 87) {
     const ajuste = ((umidadeRelativa - 85) / 10) * ((87 - temperaturaF) / 5);
     indiceCalorF += ajuste;
-  }
-
-  // Se o Indice de Calor for menor que 80°F, utiliza o modelo simplificado
-  if (indiceCalorF < 80) {
-    indiceCalorF =
-      0.5 *
-      (temperaturaF +
-        61.0 +
-        ((temperaturaF - 68.0) * 1.2) +
-        (umidadeRelativa * 0.094));
   }
 
   // Converte o resultado de volta para Celsius
